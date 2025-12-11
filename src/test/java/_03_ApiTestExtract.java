f148e2531d71312caf4b36f7c26bc5f1a2959b82
TechnoStudy <ismettechno@gmail.com>
2025-12-11 20:14:41 +0000

    extractingJsonPath3 ve soru

--- src/test/java/_03_ApiTestExtract.java
+++ src/test/java/_03_ApiTestExtract.java
@@ -2,6 +2,8 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -40,6 +42,34 @@ public class _03_ApiTestExtract {
                         .extract().path("meta.pagination.limit");
 
         Assert.assertTrue(limit==10);
+    }
+
+
+    @Test
+    public void extractingJsonPath3() {
+        // Soru : "https://gorest.co.in/public/v1/users"  endpoint in den dönen
+        // bütün id leri alınız ve içinde 8228890  olduğunu doğrulayınız
+
+        ArrayList<Integer> idler=
+                        given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .extract().path("data.id");
+
+        System.out.println("idler = " + idler);
+        Assert.assertTrue(idler.contains(8228890));
+    }
+
+    @Test
+    public void extractingJsonPath5() {
+        // Soru : "https://gorest.co.in/public/v1/users"  endpoint in den dönen
+        // bütün name leri in içiden "Bhasvan Butt" değerinin geçtiğini
+        // TestNg assertion ile doğrulayınız.
+
+
     }
 
 
